580cf4baa2cc26b805bf864df74220c196870150
jagruti nand <jagrutinand77@gmail.com>
2025-07-10 18:54:32 +0530

    udating env

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -7,9 +7,11 @@ export const StoreContext= createContext(null)
 
 
 const StoreContextProvider=(props)=>{
+  const url="http://localhost:4000";
 
   const [cartItems,setCartItems]=useState({})
-  const url="http://localhost:4000";
+  
+  
   const [token,setToken]=useState("")
 
   const [food_list,setFoodList]=useState([])
