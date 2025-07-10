3cc1ca8138440cbc670f88ec62a26b6923f4f501
jagrutnand <36583632+jagrutnand@users.noreply.github.com>
2025-07-10 19:33:49 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -8,7 +8,7 @@ const stripe =new Stripe(process.env.STRIPE_SECRET_KEY)
 
 // placing user order from frontend 
  const placeOrder = async (req,res) =>{
-    const frontend_url=process.env.FRONTEND_URL;
+    const frontend_url="https://food-order-frontend-five.vercel.app";
 
     try {
         const newOrder = new orderModel({
