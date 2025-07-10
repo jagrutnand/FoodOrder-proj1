2415eededd0321e57d7a316117ae097cf0cd9428
jagrutnand <36583632+jagrutnand@users.noreply.github.com>
2025-07-10 21:42:57 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -46,8 +46,8 @@ const stripe =new Stripe(process.env.STRIPE_SECRET_KEY)
         const session = await stripe.checkout.sessions.create({
             line_items:line_items,
             mode:'payment',
-            success_url:`${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
-            cancel_url:`${frontend_url}/verify?success=false&orderId=${newOrder._id}`,
+            success_url:`${frontend_url}verify?success=true&orderId=${newOrder._id}`,
+            cancel_url:`${frontend_url}verify?success=false&orderId=${newOrder._id}`,
         })
 
         res.json({success:true,session_url:session.url})
