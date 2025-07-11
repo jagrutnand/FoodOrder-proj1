ddea3e3a60ef2e5880754d501ae3b84eb3f3be64
jagruti nand <jagrutinand77@gmail.com>
2025-07-11 15:40:38 +0530

    changes in carousel for explore menu

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,26 +1,56 @@
 import React from 'react'
 import './ExploreMenu.css'
 import { menu_list } from '../../assets/assets'
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { Navigation } from 'swiper/modules';
 
 const ExploreMenu = ({category,setCategory}) => {
-  return (
+ return (
     <div className='explore-menu' id='explore-menu'>
-        <h1>Explore Our Menu</h1>
-        <p className='explore-menu-text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate nesciunt dolorem modi maiores illo aliquam non 
-            ipsum minima tenetur quis, odit provident dolorum.</p>
-            <div className="explore-menu-list" >
-                {menu_list.map((item,index)=>{
-                    return(
-                        <div onClick={()=>setCategory(prev=>prev===item.menu_name?"All":item.menu_name)} key={index} className="explore-menu-list-item">
-                            <img className={category===item.menu_name? "active":""} src={item.menu_image} alt="" />
-                            <p>{item.menu_name}</p>
-                        </div>
-                    )
-                })}
-            </div>
+      <h1>Explore Our Menu</h1>
+      <p className='explore-menu-text'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate nesciunt dolorem modi maiores illo aliquam non 
+        ipsum minima tenetur quis, odit provident dolorum.
+      </p>
+
+      <div className="explore-menu-list">
+        <Swiper
+          modules={[Navigation]}
+          spaceBetween={20}
+          slidesPerView={5}
+          navigation
+          breakpoints={{
+            640: { slidesPerView: 2 },
+            768: { slidesPerView: 3 },
+            1024: { slidesPerView: 4 },
+          }}
+        >
+          {menu_list.map((item, index) => (
+            <SwiperSlide key={index}>
+              <div
+                onClick={() =>
+                  setCategory(prev =>
+                    prev === item.menu_name ? "All" : item.menu_name
+                  )
+                }
+                className="explore-menu-list-item"
+              >
+                <img
+                  className={category === item.menu_name ? "active" : ""}
+                  src={item.menu_image}
+                  alt={item.menu_name}
+                />
+                <p>{item.menu_name}</p>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+
       <hr />
     </div>
-  )
+  );
 }
 
 export default ExploreMenu
