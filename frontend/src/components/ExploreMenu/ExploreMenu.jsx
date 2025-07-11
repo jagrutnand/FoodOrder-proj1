a5bfc16ab44d2eb495dcc77d8cd9831ada7dd41e
jagrutnand <36583632+jagrutnand@users.noreply.github.com>
2025-07-11 16:05:07 +0530

    Update ExploreMenu.jsx

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -23,6 +23,7 @@ const ExploreMenu = ({category,setCategory}) => {
           slidesPerView={5}
           navigation
           breakpoints={{
+            547: { slidesPerView: 2},
             640: { slidesPerView: 2 },
             768: { slidesPerView: 3 },
             1024: { slidesPerView: 4 },
